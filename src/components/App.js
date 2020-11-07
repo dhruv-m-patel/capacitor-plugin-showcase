331ba0d7fc76115bbf682297fbacefa9e5cf105c
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-07 15:59:57 -0600

    refactor: use material-ui components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import './App.css'
 
 export default function App() {
     return (
-        <div>
-            <h1>Welcome to PWA Demo App</h1>
-            <p>A PWA demo mobile app built with Capacitor.js and React</p>
-        </div>
+        <Box className="root">
+            <Typography variant="h4">Welcome to PWA Demo App</Typography>
+            <Typography>A PWA demo mobile app built with Capacitor.js and React</Typography>
+        </Box>
     )
 }
