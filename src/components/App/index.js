1664dd611a2094773611b762a2fe98af0c8919b2
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-07 23:35:05 -0600

    refactor: reorganize App component better

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Box, Typography } from '@material-ui/core'
+import CameraPluginDemo from '../CameraPluginDemo'
 import './App.css'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
         <Box className="root">
             <Typography variant="h4">Welcome to PWA Demo App</Typography>
             <Typography>A PWA demo mobile app built with Capacitor.js and React</Typography>
+            <CameraPluginDemo />
         </Box>
     )
 }
