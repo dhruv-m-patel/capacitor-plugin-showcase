ba37422bd691941f1590971acb266c9fc20f1b7e
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-08 18:02:23 -0600

    refactor: reorganize plugin demos

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Box, Typography } from '@material-ui/core'
-import CameraPluginDemo from '../CameraPluginDemo'
-import BrowserPluginDemo from '../BrowserPluginDemo'
+import CameraPlugin from '../capacitor-plugins/Camera'
+import BrowserPlugin from '../capacitor-plugins/Browser'
 import './App.css'
 
 export default function App() {
@@ -10,8 +10,8 @@ export default function App() {
             <Box className="root">
                 <Typography variant="h4">Welcome to PWA Demo App</Typography>
                 <Typography>A PWA demo mobile app built with Capacitor.js and React</Typography>
-                <CameraPluginDemo />
-                <BrowserPluginDemo />
+                <CameraPlugin />
+                <BrowserPlugin />
             </Box>
         </Container>
     )
--- src/components/capacitor-plugins/Browser.js
+++ src/components/capacitor-plugins/Browser.js
@@ -1,12 +1,12 @@
 import React, { useCallback } from 'react'
 import { Plugins } from '@capacitor/core'
-import Section from '../Section'
 import { Button } from '@material-ui/core'
+import Section from '../Section'
 
 export default function BrowserPluginDemo() {
     const openBrowser = useCallback(async () => {
         const { Browser } = Plugins
-        await Browser.open({ url: 'https://ecompliance.com' })
+        await Browser.open({ url: 'https://capacitorjs.com/docs/apis' })
     }, [])
 
     return (
