c1d4c2876df1c465a2c7f5836fc3d43edd5cc93d
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-08 20:22:52 -0600

    update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Box, Typography } from '@material-ui/core'
+import { Container, Typography } from '@material-ui/core'
 import { Capacitor } from '@capacitor/core'
 import AccessibilityPlugin from '../capacitor-plugins/Accessibility'
 import AppPlugin from '../capacitor-plugins/App'
@@ -12,27 +12,25 @@ import './App.css'
 
 export default function App() {
     return (
-        <Container>
-            <Box className="root">
-                <Typography variant="h4">Welcome to PWA Demo App</Typography>
-                <Typography variant="subtitle1">A PWA demo mobile app built with Capacitor.js and React</Typography>
-                <br />
-                <Typography variant="subtitle2">
-                    The purpose of the app is to demonstrate functionality offered by the official Capacitor Plugins
-                </Typography>
-                <br />
-                {!Capacitor.isNative && (
-                    <Section header title="Plugin">
-                        Available Actions
-                    </Section>
-                )}
-                <AccessibilityPlugin />
-                <AppPlugin />
-                <BackgroundTaskPlugin />
-                <BrowserPlugin />
-                <CameraPlugin />
-                <ClipboardPlugin />
-            </Box>
+        <Container className="root">
+            <Typography variant="h4">Welcome to PWA Demo App</Typography>
+            <Typography variant="subtitle1">A PWA demo mobile app built with Capacitor.js and React</Typography>
+            <br />
+            <Typography variant="subtitle2">
+                The purpose of the app is to demonstrate functionality offered by the official Capacitor Plugins
+            </Typography>
+            <br />
+            {!Capacitor.isNative && (
+                <Section header title="Plugin">
+                    Available Actions
+                </Section>
+            )}
+            <AccessibilityPlugin />
+            <AppPlugin />
+            <BackgroundTaskPlugin />
+            <BrowserPlugin />
+            <CameraPlugin />
+            <ClipboardPlugin />
         </Container>
     )
 }
