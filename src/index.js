0efb05464ad5322226cb52a8f475244b37848d57
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2020-11-08 20:24:48 -0600

    refactor: remove redux, not needed

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { defineCustomElements } from '@ionic/pwa-elements/loader'
-import ReduxStateDecorator from './redux/StateDecorator'
 
 import App from './components/App'
 
 export default function renderApp() {
-    let preloadedState
-    if (typeof window !== 'undefined' && window.__PRELOADED_STATE__) {
-        preloadedState = window.__PRELOADED_STATE__
-        const stateData = document.getElementById('stateData')
-        document.head.removeChild(stateData)
-        delete window.__PRELOADED_STATE__
-    }
-    ReactDOM.render(
-        <ReduxStateDecorator initialState={preloadedState}>
-            <App />
-        </ReduxStateDecorator>,
-        document.getElementById('root')
-    )
+    ReactDOM.render(<App />, document.getElementById('root'))
     if (typeof window !== 'undefined') {
         defineCustomElements(window)
     }
